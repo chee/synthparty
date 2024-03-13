809f4bd94a529fefe3cfef0ec219ce1820590f8d
chee <yay@chee.party>
2024-03-13 18:21:21 +0000

    add new colours

--- public/drum/lib/colours.js
+++ public/drum/lib/colours.js
@@ -1,10 +1,20 @@
 export default [
-	"#33ff72",
+	"#00fF91",
 	"#33ffff",
 	"#ff2a75",
 	"#ffff33",
 	"#ffaa69",
-	"#ff3369",
-	"#fcf",
-	"#12ccff"
+	"#df3379",
+	"#ffccff",
+	"#12ccff",
+	"#006AFF",
+	"#00875A",
+	"#EB1800",
+	"#ffffcc",
+	"#ffca00",
+	"#00ffcc",
+	"#9966ff",
+	"#ff66bb",
+	"#cc99ba",
+	"#cc99ff"
 ]
