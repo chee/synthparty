6539d1cd8e597058832683f5c037be014e259ab9
chee <yay@chee.party>
2024-03-13 18:29:27 +0000

    configure new sound bindings

--- public/drum/elements/sound-element.js
+++ public/drum/elements/sound-element.js
@@ -1,15 +1,20 @@
 import {partyElements, PartyElement} from "./party-elements.js"
-import globalStyles from "./global-styles.js"
 import Sound from "../sound.js"
 
 export default class DelugeSound extends PartyElement {
 	constructor() {
 		super()
-		this.shadowRoot.adoptedStyleSheets = [globalStyles]
-		this.$("#audition").addEventListener("click", () => this.sound.audition())
-		this.$("#browse").addEventListener("click", () =>
-			this.announce("browse", this.sound.index)
-		)
+		this.$("#audition").addEventListener("click", () => {
+			this.announce("audition")
+			this.sound.audition()
+		})
+		this.$("#browse").addEventListener("click", async () => {
+			let [sound] = await Sound.browse({
+				multiple: false
+			})
+			this.sound.replace(sound)
+		})
+
 		this.$("#up").addEventListener("click", () =>
 			this.announce("move-up", this.sound.index)
 		)
@@ -27,6 +32,21 @@ export default class DelugeSound extends PartyElement {
 		this.$("#loop-mode").addEventListener("change", event => {
 			this.sound.loopMode = event.target.value
 		})
+		this.$("#choke").addEventListener("change", event => {
+			this.sound.choke = event.target.checked
+		})
+		this.$("#reversed").addEventListener("change", event => {
+			this.sound.reversed = event.target.checked
+		})
+		this.$("#sidechain-send").addEventListener("change", event => {
+			this.sound.sidechainSend = event.target.checked
+		})
+		this.$("#linear-interpolation").addEventListener("change", event => {
+			this.sound.linearInterpolation = event.target.checked
+		})
+		this.$("#time-stretch").addEventListener("change", event => {
+			this.sound.timeStretch = event.target.checked
+		})
 	}
 
 	/** @type {Sound} */
@@ -37,8 +57,15 @@ export default class DelugeSound extends PartyElement {
 	set sound(sound) {
 		this.set("sound", sound, () => {
 			this.$("#name").value = sound.name
-			this.$("#audition").color = sound.color
+			this.$("#audition").style.background = sound.color
+			this.style.border = `1px solid ${sound.color}`
 			this.$("#loop-mode").value = sound.loopMode
+			this.$("#choke").checked = sound.choke
+			this.$("#reversed").checked = sound.reversed
+			this.$("#sidechain-send").checked = sound.sidechainSend
+			this.$("#linear-interpolation").checked = sound.linearInterpolation
+			this.$("#time-stretch").checked = sound.timeStretch
+			this.$("#slot").textContent = sound.index.toString().padStart(3, "0")
 		})
 	}
 }
